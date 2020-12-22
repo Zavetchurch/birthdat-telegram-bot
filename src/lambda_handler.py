4c2d6a1c8aa661c6a902cae1a8f878ed423eb6ba
belgabad1 <tima.babashev@gmail.com>
2020-12-22 13:41:22 +0300

    Add oauth2client to requirements; fix some style moments

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -13,16 +13,16 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-BIRTHDAY_KEY = "Дата рождения"
-NAME_KEY = "ФИО"
+BIRTHDAY_KEY = 'Дата рождения'
+NAME_KEY = 'ФИО'
 
 
-def get_telegram_chats():
-    config_object = s3.get_object("telegram-chats.json")
+def get_telegram_chats() -> dict:
+    config_object = s3.get_object('telegram-chats.json')
     return json.loads(config_object)
 
 
-def is_birthday(record: Dict):
+def is_birthday(record: Dict) -> bool:
     try:
         birthday_date = datetime.strptime(record[BIRTHDAY_KEY], '%Y-%m-%d')
         return birthday_date.day == datetime.today().day and birthday_date.month == datetime.today().month
@@ -35,8 +35,8 @@ def lambda_handler(request, context):
     pusher: PusherBot = PusherBot()
     chats = get_telegram_chats()
 
-    for chat in chats["chats"]:
-        records: List = spreadsheet.get_records_from_sheet(chat["sheetname"], chat["page"])
+    for chat in chats['chats']:
+        records: List = spreadsheet.get_records_from_sheet(chat['sheetname'], chat['page'])
         birthdays = filter(is_birthday, records)
         for birthday in birthdays:
-            pusher.push_notification(chat["celebratePattern"].format(birthday[NAME_KEY]), chat["id"])
+            pusher.push_notification(chat['celebratePattern'].format(birthday[NAME_KEY]), chat['id'])
